168888f6049e66bb5f5b539cc102cae3dd2f6cd0
HETASVI <hetasvighevariya8@gmail.com>
2026-07-06 05:56:50 +0000

    summery

---
+++ basics01/01_datatypeSummary.js
@@ -0,0 +1,35 @@
+// Primitive
+// 7 types : String , Number , Boolean , null , undefined , symbol , BigInt
+
+// const score = 100;
+// const scoreValue = 27.03
+
+// const isLoggedIn = true
+// const outsideTemp = null 
+// let userEmail;
+
+// const id = Symbol("3368")
+// const pass = Symbol("3368")
+
+// console.log(id === pass);
+
+// const bigNumber = 1234567890n
+
+
+// Reference (Non Primitive)
+// Array , Object , functions
+
+const heros = [ "superman" , "spiderman" , "ironman" ]
+
+let myObj = {
+    name : "superman",
+    age : 376
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof heros)
+console.log(typeof myObj)
+console.log(typeof myFunction)
