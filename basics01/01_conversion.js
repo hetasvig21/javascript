f3b455e6aefd53358485d703023bf49a01ec8ac2
HETASVI <hetasvighevariya8@gmail.com>
2026-07-06 05:02:46 +0000

    conversion

---
+++ basics01/01_conversion.js
@@ -0,0 +1,38 @@
+// let age = "55"
+// console.log(typeof age);
+// console.log(typeof(age));
+
+// let valueNumber = Number(age);
+
+// console.log(typeof valueNumber)
+
+
+// let score = "55A"
+// console.log(typeof score);
+
+// let valueScore = Number(score)
+// console.log(typeof valueScore)
+// console.log(valueScore); // NaN Not a Number
+
+// let score = null
+// console.log(typeof score); //object
+
+// let valueScore = Number(score)
+// console.log(typeof valueScore)
+// console.log(valueScore); // 0
+
+// let score = undefined
+// console.log(typeof score); // undefined
+
+// let valueScore = Number(score)
+// console.log(typeof valueScore)
+// console.log(valueScore);  // NaN 
+
+// "33" = 33
+// "3uftd" = NaN
+// true => 1 , false => 0
+
+let isLoggedIn = "" // "" => false ,1 => true , "duywqjt" => true , 0 => false
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
