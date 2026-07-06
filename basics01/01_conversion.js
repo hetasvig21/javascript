2705747c540089a66d6ac964656e0bb2755afd4b
HETASVI <hetasvighevariya8@gmail.com>
2026-07-06 05:23:04 +0000

    conversion operation

--- basics01/01_conversion.js
+++ basics01/01_conversion.js
@@ -1,7 +1,6 @@
 // let age = "55"
 // console.log(typeof age);
 // console.log(typeof(age));
-
 // let valueNumber = Number(age);
 
 // console.log(typeof valueNumber)
@@ -32,7 +31,43 @@
 // "3uftd" = NaN
 // true => 1 , false => 0
 
-let isLoggedIn = "" // "" => false ,1 => true , "duywqjt" => true , 0 => false
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = "" // "" => false ,1 => true , "duywqjt" => true , 0 => false
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
+// -------------operation--------------
+
+// let value = 3
+// let negValue = -value 
+// console.log(negValue);  // -3
+
+
+// console.log(3+3)
+// console.log(3-3)
+// console.log(3*3)
+// console.log(3/3)
+// console.log(3**3)
+// console.log(3%3)
+
+// let str1 = "hello"
+// let str2 = " world"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 3);
+// console.log(1 + "3");
+// console.log("1" + 3 + 3);
+// console.log(1 + 3 + "4");
+
+
+// console.log(+true); //1
+// console.log(+""); // 0
+
+
+// let num1 , num2 , num3 ;
+// num1 = num2 = num3 = 4+4;
+
+
+let gameCounter = 4;
+gameCounter++
+console.log(gameCounter);
