62646d0f9361db24facf69f4ac752433baf5c791
HETASVI <hetasvighevariya8@gmail.com>
2026-07-07 04:27:33 +0000

    nums

---
+++ basics01/01_numAndMath.js
@@ -0,0 +1,28 @@
+// const score = 100;
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance);
+
+// const otherNumber = 23.7687
+// console.log(otherNumber.toPrecision(3));
+
+// const otherNumber1 = 123.7687
+// console.log(otherNumber1.toPrecision(3));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ++++++++++++++++ MATH ++++++++++++++++++
+
+// console.log(Math.random())
+// console.log(Math.random()*6)
+// console.log(Math.random()*10)
+// console.log(Math.random()*3)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
