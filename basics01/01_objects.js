7725b31bb5ec29b1817c0466862c62da436bd3e9
HETASVI <hetasvighevariya8@gmail.com>
2026-07-08 15:41:31 +0000

    add

--- basics01/01_objects.js
+++ basics01/01_objects.js
@@ -9,16 +9,35 @@ const user = {
     age : 32,
     city : "surat",
     [mySym] : "myKey1",
-    "full name" : "cvbnm"
+    "full name" : "cvbnm",
+    email : "fdghfsj@gmail.com"
 }
-
-console.log(user.name);
+const user1 = {
+    name : "sgzf",
+    age : 32,
+    city : "surat",
+    [mySym] : "myKey1",
+    "full name" : "cvbnm",
+    email : "fdghfsj@gmail.com"
+}
+// console.log(user.name);
 // console.log(user.full name) // give arr 
-console.log(user["full name"]);
-console.log(user[mySym]);
+// console.log(user["full name"]);
+// console.log(user[mySym]);
 
+// user.email = "fdghfsj@chatgpt.com"
+// console.log(user)
+// Object.freeze(user) // after freeze you can not change any value
+// user.email = "fdghfsj@micro.com"
+// console.log(user)
 
+user.greeting = function(){
+    console.log("hello user");
+}
+user.greeting1 = function(){
+    console.log(`hello user , ${this.name}`);
+}
+console.log(user)
+console.log(user.greeting())
+console.log(user.greeting1())
 
-// const newUser = new Object(
-//     name = "tydwq"
-// )
