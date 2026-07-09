c8d2a294c4a6045bb161945ff07f93df0e5a6f8c
HETASVI <hetasvighevariya8@gmail.com>
2026-07-09 04:56:10 +0000

    obj2

---
+++ basics01/02_objects.js
@@ -0,0 +1,32 @@
+// const tinderuser = new Object();
+
+// const tinderuser = {}
+// console.log(tinderuser);
+
+// tinderuser.email = "some@gmail.com";
+// tinderuser.pass = "23456789";
+// console.log(tinderuser);
+
+
+// const regularuser = {
+//     email: "anyone@gmail.com",
+//     fullname: {
+//         name: {
+//             firstname: "joan",
+//             lastname: "sana"
+//         }
+//     }
+// }
+/////////
+// const regularuser = {
+//     email: "anyone@gmail.com",
+//     fullname: {
+//             firstname: "joan",
+//             lastname: "sana"
+//     }
+// }
+
+// console.log(regularuser.fullname.name?.firstname);
+///////////
+
+
