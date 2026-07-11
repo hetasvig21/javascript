7022bc9231f91380481265c81d003098b61a5f64
HETASVI <hetasvighevariya8@gmail.com>
2026-07-11 03:59:09 +0000

    obj add

--- basics01/02_objects.js
+++ basics01/02_objects.js
@@ -1,15 +1,15 @@
 // const tinderuser = new Object();
 
-const tinderuser = {}
+// const tinderuser = {}
 // console.log(tinderuser);
 
-tinderuser.email = "some@gmail.com";
-tinderuser.pass = "23456789";
-console.log(tinderuser);
+// tinderuser.email = "some@gmail.com";
+// tinderuser.pass = "23456789";
+// console.log(tinderuser);
 
-console.log(Object.keys(tinderuser));
-console.log(Object.values(tinderuser));
-console.log(Object.entries(tinderuser));
+// console.log(Object.keys(tinderuser));
+// console.log(Object.values(tinderuser));
+// console.log(Object.entries(tinderuser));
 
 // const regularuser = {
 //     email: "anyone@gmail.com",
@@ -32,8 +32,8 @@ console.log(Object.entries(tinderuser));
 // console.log(regularuser.fullname.name?.firstname);
 ///////////
 
-const obj1 = {1:'a',2:'b'}
-const obj2 = {3:'a',4:'b'}
+// const obj1 = {1:'a',2:'b'}
+// const obj2 = {3:'a',4:'b'}
 
 // const obj3 = Object.assign(obj1,obj2)  // here obj1 is target and remaining all source
 // console.log(obj3); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
@@ -53,20 +53,35 @@ const obj2 = {3:'a',4:'b'}
 // console.log(obj3);
 // console.log(obj1);
 
-const user = [
-    {
-        id : 1,
-        name : "qwer"
-    },
-    {
-        id : 2,
-        name : "qwer"
-    },
-    {
-        id : 3,
-        name : "qwer"
-    },
-]
+// const user = [
+//     {
+//         id : 1,
+//         name : "qwer"
+//     },
+//     {
+//         id : 2,
+//         name : "qwer"
+//     },
+//     {
+//         id : 3,
+//         name : "qwer"
+//     },
+// ]
 
 // console.log(user[1].id);
+
+const course = { 
+    coursename : "JS",
+    price : "999",
+    courseInstructor : "asdfg"
+}
+
+// course.courseInstructor
+// instead of
+// destructuring
+// const {courseInstructor} = course;
+// console.log(courseInstructor);
+
+// const {courseInstructor : instructor} = course ; 
+// console.log(instructor);
 
