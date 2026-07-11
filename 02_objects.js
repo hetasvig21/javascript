4f8a59a8f08899a8e37dc015cb10a5ec1e9373ce
HETASVI <hetasvighevariya8@gmail.com>
2026-07-11 04:09:49 +0000

    change

--- 02_objects.js
+++ 02_objects.js
@@ -70,11 +70,11 @@
 
 // console.log(user[1].id);
 
-const course = { 
-    coursename : "JS",
-    price : "999",
-    courseInstructor : "asdfg"
-}
+// const course = { 
+//     coursename : "JS",
+//     price : "999",
+//     courseInstructor : "asdfg"
+// }
 
 // course.courseInstructor
 // instead of
@@ -85,3 +85,6 @@ const course = {
 // const {courseInstructor : instructor} = course ; 
 // console.log(instructor);
 
+
+// json 
+// {"results":[{"gender":"female","name":{"title":"Miss","first":"Rocío","last":"Pizarro"},"location":{"street":{"number":8822,"name":"Calle Baja California Sur"},"city":"Puerta de Andaracua","state":"Aguascalientes","country":"Mexico","postcode":68120,"coordinates":{"latitude":"23.7102","longitude":"34.0958"},"timezone":{"offset":"-5:00","description":"Eastern Time (US & Canada), Bogota, Lima"}},"email":"rocio.pizarro@example.com","login":{"uuid":"db14e290-a776-40ee-96ae-5bd3b12a0758","username":"whitefrog718","password":"oooo","salt":"EEAd45Ky","md5":"e72938a3034208cf0b1d2609a4e6aa13","sha1":"1e6104137d34c66a4a76505a720ae831896f2b75","sha256":"26be463e3c7990843b2dd74efe2f2e503b06c52584f82664b87d06fbeb582b89"},"dob":{"date":"1993-04-04T08:04:49.654Z","age":33},"registered":{"date":"2006-08-26T08:00:32.247Z","age":19},"phone":"(611) 604 3570","cell":"(602) 282 9898","id":{"name":"NSS","value":"17 33 21 7061 3"},"picture":{"large":"https://randomuser.me/api/portraits/women/68.jpg","medium":"https://randomuser.me/api/portraits/med/women/68.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/women/68.jpg"},"nat":"MX"}],"info":{"seed":"2254e1e33bd9e88e","results":1,"page":1,"version":"1.4"}}
